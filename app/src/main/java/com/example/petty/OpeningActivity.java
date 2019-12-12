c558d8ae521279e277cacd3f7dafe439bf78c203
kencomboizdang <55649474+kencomboizdang@users.noreply.github.com>
2019-12-12 13:33:40 +0700

    13:30 - 12/12
    Sumbit Layout

---
+++ app/src/main/java/com/example/petty/OpeningActivity.java
@@ -0,0 +1,26 @@
+package com.example.petty;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class OpeningActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_opening);
+        Handler handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(OpeningActivity.this, LoginActivity.class);
+                startActivity(intent);
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+                finish();
+            }
+        },1800);
+    }
+}
