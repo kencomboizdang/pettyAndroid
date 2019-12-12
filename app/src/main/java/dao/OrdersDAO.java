1cdc37fef1f380dc4bdfce6adfac6aa247a8c97d
Nhunguyen181 <45573915+Nhunguyen181@users.noreply.github.com>
2019-12-12 16:49:53 +0700

    Update OrdersDAO.java

--- app/src/main/java/dao/OrdersDAO.java
+++ app/src/main/java/dao/OrdersDAO.java
@@ -1,4 +1,19 @@
 package dao;
 
-public class OrdersDAO {
+import android.view.View;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.io.Serializable;
+
+public class OrdersDAO implements Serializable {
+    FirebaseStorage firebaseStorage;
+    StorageReference storageReference;
+
+    public void ClickToDelete(String orderId) {
+        FirebaseDatabase.getInstance().getReference("users").child(orderId).removeValue();
+    }
 }
