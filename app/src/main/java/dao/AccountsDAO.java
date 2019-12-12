19b2af0fba3a27485a5167c8531cf7a20e22c9b8
hnlinh2112 <58517019+hnlinh2112@users.noreply.github.com>
2019-12-12 10:16:39 +0700

    Update AccountsDAO.java

--- app/src/main/java/dao/AccountsDAO.java
+++ app/src/main/java/dao/AccountsDAO.java
@@ -11,20 +11,16 @@ public class AccountsDAO implements Serializable {
 
     private static final String ACCOUNT = "accounts";
 
-    public AccountsDTO createAccount(AccountsDTO accounts) {
-        try {
-            if (accounts != null) {
-                DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference(ACCOUNT);
-                String accountId = mDatabase.push().getKey();
-                accounts = new AccountsDTO(accountId, "Linh", "123456", "user");
-                mDatabase.child(accountId).setValue(accounts);
-                return accounts;
-            }
-            return null;
-        } catch (Exception e) {
-            return null;
-        }
+    public AccountsDTO createAccount(AccountsDTO dto) {
+        DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference(ACCOUNT);
+        String accountId = mDatabase.push().getKey();
+        mDatabase.child(accountId).setValue(dto);
+        return dto;
     }
 
-    //public boolean updatePassword()
+//    public AccountsDTO updateAccount(AccountsDTO dto, String newPassword) {
+//        DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference(ACCOUNT);
+//        mDatabase.child("-LvrmQFeXMwONfN1TBXz").child("password").setValue(newPassword);
+//        return dto;
+//    }
 }
