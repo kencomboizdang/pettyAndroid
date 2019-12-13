5002795f34c0fbdff6949f74b0a6bf1a52ad84db
hnlinh2112 <58517019+hnlinh2112@users.noreply.github.com>
2019-12-13 14:13:13 +0700

    13/12/2019 14h15

--- app/src/main/java/dao/AccountsDAO.java
+++ app/src/main/java/dao/AccountsDAO.java
@@ -1,30 +1,22 @@
-package dao;
-
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-
-import java.io.Serializable;
-
-import dto.AccountsDTO;
-
-public class AccountsDAO implements Serializable {
-
-    private static final String ACCOUNT = "accounts";
-
-    public AccountsDTO createAccount(AccountsDTO accounts) {
-        try {
-            if (accounts != null) {
-                DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference(ACCOUNT);
-                String accountId = mDatabase.push().getKey();
-                accounts = new AccountsDTO(accountId, "Linh", "123456", "user");
-                mDatabase.child(accountId).setValue(accounts);
-                return accounts;
-            }
-            return null;
-        } catch (Exception e) {
-            return null;
-        }
-    }
-
-    //public boolean updatePassword()
-}
+//package dao;
+//
+//import com.google.firebase.database.DatabaseReference;
+//import com.google.firebase.database.FirebaseDatabase;
+//
+//import java.io.Serializable;
+//
+//import dto.AccountsDTO;
+//
+//public class AccountsDAO implements Serializable {
+//
+//    private static final String ACCOUNT = "accounts";
+//
+//    public AccountsDTO createAccount(AccountsDTO accounts) {
+//        DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference(ACCOUNT);
+//        String accountId = mDatabase.push().getKey();
+//        accounts.setId(accountId);
+//        mDatabase.child(accountId).setValue(accounts);
+//        return accounts;
+//    }
+//
+//}
