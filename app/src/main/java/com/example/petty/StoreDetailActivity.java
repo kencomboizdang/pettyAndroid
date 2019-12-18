f9d8ac263009b73e6f6f9e88a561f26544070a7c
hnlinh2112 <58517019+hnlinh2112@users.noreply.github.com>
2019-12-18 15:51:37 +0700

    18/12/2019 15h51

--- app/src/main/java/com/example/petty/StoreDetailActivity.java
+++ app/src/main/java/com/example/petty/StoreDetailActivity.java
@@ -1,31 +1,32 @@
 package com.example.petty;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.nfc.Tag;
 import android.os.Bundle;
-import android.provider.ContactsContract;
 import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.fragment.app.FragmentActivity;
+
 import com.bumptech.glide.Glide;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-import dto.StoresDTO;
+public class StoreDetailActivity extends FragmentActivity implements OnMapReadyCallback {
 
-public class StoreDetailActivity extends AppCompatActivity {
-
-    TextView txtNameStore, txtAddress, txtAdd2;
     private static final String STORES = "stores";
+    TextView txtNameStore, txtAddress, txtAdd2;
     DatabaseReference mDatabase;
     ImageView imgStore;
 
     String id = "";
+    GoogleMap ggMap;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,25 +38,21 @@ public class StoreDetailActivity extends AppCompatActivity {
         txtAddress = (TextView) findViewById(R.id.txtAddress);
         imgStore = (ImageView) findViewById(R.id.imgStore);
         txtAdd2 = (TextView) findViewById(R.id.txtAdd2);
-
-        mDatabase.addValueEventListener(new ValueEventListener() {
+        Bundle bundle = getIntent().getBundleExtra("dataStore");
+        id = bundle.getString("id_store");
+        Log.d("TAG", id);
+        mDatabase.child(id).addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                for(DataSnapshot noteSnapshot : dataSnapshot.getChildren()) {
-                    for(DataSnapshot ds : dataSnapshot.getChildren()) {
-                        StoresDTO dto = ds.child(id).getValue(StoresDTO.class);
-                        String name = ds.child("name").getValue(String.class);
-                        String ward = ds.child("ward").getValue(String.class);
-                        String district = ds.child("district").getValue(String.class);
-                        String provine = ds.child("province").getValue(String.class);
-                        String img = ds.child("logoImg").getValue(String.class);
-                        txtNameStore.setText(name);
-                        txtAddress.setText(ward + ", " + ", " + district + ", " + provine);
-                        txtAdd2.setText(ward + ", " + ", " + district + ", " + provine);
-                        Glide.with(StoreDetailActivity.this).load(img).into(imgStore);
-                    }
-                }
-
+                String name = dataSnapshot.child("name").getValue(String.class);
+                String ward = dataSnapshot.child("ward").getValue(String.class);
+                String district = dataSnapshot.child("district").getValue(String.class);
+                String provine = dataSnapshot.child("province").getValue(String.class);
+                String img = dataSnapshot.child("logoImg").getValue(String.class);
+                txtNameStore.setText(name);
+                txtAddress.setText(ward + ", " + ", " + district + ", " + provine);
+                txtAdd2.setText(ward + ", " + ", " + district + ", " + provine);
+                Glide.with(StoreDetailActivity.this).load(img).into(imgStore);
             }
 
             @Override
@@ -63,12 +60,13 @@ public class StoreDetailActivity extends AppCompatActivity {
 
             }
         });
-        loadStore();
+        // google map
+
+        SupportMapFragment mapFragment;
     }
 
-    public void loadStore() {
-        Bundle bundle = getIntent().getBundleExtra("dataStore");
-        id = bundle.getString("id_store");
-        Log.d("TAG", id);
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
     }
 }
